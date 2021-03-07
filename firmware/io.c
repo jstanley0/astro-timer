6142c2f5bfea98da2a2acbfe1659d1ca3baff8da
Jeremy Stanley <jeremy@instructure.com>
2021-03-07 07:46:02 -0700

    support discrete encoder
    the current hardware uses an encoder on a breakout board with
    discrete pullup resistors, but the planned update (and PCB
    currently in hardware/) uses a discrete encoder on the same
    board. I've verified that it works with the AVR's integrated
    pullup resistors

--- firmware/io.c
+++ firmware/io.c
@@ -8,7 +8,11 @@ void io_init()
     DDRB  = 0b00111111;
     PORTB = 0b00000000;
     DDRC  = 0b00000000;
+#ifdef EXTERNAL_ENCODER_PULLUPS
     PORTC = 0b00111000;
+#else
+    PORTC = 0b00111111;
+#endif
     DDRD  = 0b11111111;
     PORTD = 0b11111111;
 }
--- firmware/io.h
+++ firmware/io.h
@@ -17,6 +17,8 @@ void io_init();
 // PORTC5    (input)  = Start key
 // PORTD0..7 (output) = Segment cathodes (PD7 = A, PD6 = B, ... PD0 = DP)
 
+// undefine when using a discrete encoder (vs breakout board with integrated pullup resistors)
+#define EXTERNAL_ENCODER_PULLUPS
 
 // for portability, please put all explicit port references here and init_io()
 #define DIGITS_OFF()   PORTB &= 0b11100000;
