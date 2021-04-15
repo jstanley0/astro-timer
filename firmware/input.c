4422e9e826b3f860d53ec99412260784658ff42c
Jeremy Stanley <jeremy@instructure.com>
2021-04-14 22:23:13 -0600

    find an encoder-reading ISR that works better than mine
    still see an occasional missed click but on the whole
    it seems to be a lot cleaner

--- firmware/input.c
+++ firmware/input.c
@@ -1,10 +1,8 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <avr/pgmspace.h>
 #include "input.h"
 #include "io.h"
-
-// blackout time in units of 4 microseconds
-#define BOUNCE_CYCLES 5000
 
 void input_init()
 {
@@ -12,8 +10,8 @@ void input_init()
     TCCR1B = (1 << WGM12) | (1 << CS11); // start timer at 1/8 prescaler, in CTC mode
     TIMSK1 = (1 << OCIE1A);              // enable compare match A interrupt
 
-    // enable pin-change interrupt on encoder clock
-    PCMSK1 = (1 << PCINT9);
+    // enable pin-change interrupt on encoder inputs
+    PCMSK1 = (1 << PCINT8) | (1 << PCINT9);
     PCICR = (1 << PCIE1);
 }
 
@@ -27,33 +25,31 @@ ISR(TIMER1_COMPA_vect)
     input_ready = 1;
 }
 
-// end the blackout period for the encoder interrupt
-ISR(TIMER1_COMPB_vect)
-{
-    PCICR |= (1 << PCIE1);
-    TIMSK1 &= ~(1 << OCIE1B);
-    prev_clock = (PINC & 2);
-}
+// the following ISR is taken from
+// https://chome.nerpa.tech/mcu/rotary-encoder-interrupt-service-routine-for-avr-micros/
 
-// triggers when the encoder is moved
+/* encoder routine. Expects encoder with four state changes between detents */
+/* and both pins open on detent */
 ISR(PCINT1_vect)
 {
-    uint8_t clock = (PINC & 2);
-    // detect falling edge
-    if (prev_clock && !clock) {
-        encoder_ticks += (PINC & 1) ? -1 * ENCODER_MULTIPLIER : ENCODER_MULTIPLIER;
-
-        // debounce: turn off the pin-change interrupt for awhile
-        uint16_t blackout_end = TCNT1 + BOUNCE_CYCLES;
-        if (blackout_end >= OCR1A)
-            blackout_end -= OCR1A;
-        OCR1B = blackout_end;
-        PCICR &= ~(1 << PCIE1);
-        TIMSK1 |= (1 << OCIE1B);
-    }
-    prev_clock = clock;
+  static uint8_t old_AB = 3;  //lookup table index
+  static int8_t encval = 0;   //encoder value
+  static const int8_t enc_states [] PROGMEM =
+  {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};  //encoder lookup table
+  /**/
+  old_AB <<=2;  //remember previous state
+  old_AB |= ( PINC & 0x03 );
+  encval += pgm_read_byte(&(enc_states[( old_AB & 0x0f )]));
+  /* post "Navigation forward/reverse" event */
+  if( encval > 3 ) {  //four steps forward
+    encoder_ticks -= ENCODER_MULTIPLIER;
+    encval = 0;
+  }
+  else if( encval < -3 ) {  //four steps backwards
+    encoder_ticks += ENCODER_MULTIPLIER;
+    encval = 0;
+  }
 }
-
 
 // here's how button presses work:
 // - a press is registered when a button is released.
